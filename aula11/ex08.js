1cab636af97b544950b72baa9dba2cb4d8050fcb
Marcus Tomazelli <mvctomazelli@gmail.com>
2024-04-26 16:33:48 -0400

    criando sites com js

---
+++ aula11/ex08.js
@@ -0,0 +1,10 @@
+var vel = 60
+
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { 
+    console.log(`você ultrapassou a velocidade permitida. multado!!`)
+} else {
+    console.log(`dirija com cuidado e utilize o cinto de segurança!`)
+}
+
---
+++ aula11/ex09.js
@@ -0,0 +1,9 @@
+var time = 'Palmeiras'
+
+console.log(`Você torce pro ${time}`)
+
+if (time == 'Palmeiras'){
+    console.log('Você não tem mundial!')
+} else{
+    console.log('Parabéns, você tem mundial!')
+}
